5c78c654de0d565115ee84c58b5fdd460665d395
housemouse62 <ryan.skeels@gmail.com>
2026-05-06 16:46:24 +0200

    add comment count to post api

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,6 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import formatDate from "../utils/formatData";
 
 function Blog() {
+  const [posts, setPosts] = useState([]);
+
   useEffect(() => {
     const fetchposts = async () => {
       const url = "http://localhost:3000/posts/";
@@ -12,12 +15,29 @@ function Blog() {
 
         const result = await response.json();
         console.log(result);
+        setPosts(result);
       } catch (error) {
         console.error(error.message);
       }
     };
     fetchposts();
   }, []);
+
+  return (
+    <>
+      <main className="posts-main">
+        <h1>The Blog</h1>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h2 className="post-title">{post.title}</h2>
+            <p className="post-body">{post.postbody.slice(0, 200)}...</p>
+            <p className="post-time">{formatDate(post.posttime)}</p>
+            <p className="post-comments">comments: {post._count.comments}</p>
+          </div>
+        ))}
+      </main>
+    </>
+  );
 }
 
 export default Blog;
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,3 +1,46 @@
-function Login() {}
+function Login() {
+  return (
+    <>
+      <div className="login-div">
+        <form className="login-form" method="POST" action="/login">
+          <div className="form-fields">
+            <div className="login-user">
+              <h1>User Login</h1>
+            </div>
+            <div class="form-field">
+              <label for="email" className="form-label">
+                E-mail Address
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                class="form-input"
+                placeholder="thoughts@thoughtwindows.com"
+                required
+              />
+            </div>
+            <div className="form-field">
+              <label for="password" className="form-label">
+                Password:
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                class="form-input"
+                placeholder="password123!"
+                required
+              />
+            </div>
+          </div>
+          <button type="submit" class="form-button">
+            Log In
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
 
 export default Login;
